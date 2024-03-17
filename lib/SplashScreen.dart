b6fc369be2b500a037283b1f4dba1f7d905d26ac
Nimra <inimra123@gmail.com>
2024-03-17 15:24:38 +0500

    first code

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'LoginScreen.dart'; // Ensure this import points to the correct file path
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image(
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            image: AssetImage('assets/images (8).jfif'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black,
+                  Colors.black.withOpacity(0.8),
+                  Colors.black.withOpacity(0.15),
+                  Colors.black.withOpacity(0.5),
+                ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Image(
+              height: 300,
+              width: 300,
+              image: AssetImage('assets/icon.png'),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 32.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white, // Button color
+                  onPrimary: Colors.black, // Text color
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    inherit: true, // Ensure consistency in the "inherit" property
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
+                child: Text('Tap to Continue'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
